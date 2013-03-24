7ed97eace87c34a1bac8fe955adff4ba0ae69f88
Myriam Tremblay <Myriam@MacBook-Pro-de-Myriam.local>
2013-03-24 14:31:13 -0400

    Initial Commit

---
+++ TP/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TP
+//
+//  Created by Myriam Tremblay on 2013-03-24.
+//  Copyright (c) 2013 Myriam Tremblay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
